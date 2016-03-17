0afb29b6fcf1d84e7305ed43b12bbe4a6a5e5f0f
Asia <wampir1408@users.noreply.github.com>
2016-03-17 13:52:43 +0100

    Delete Account.java

--- src/Account.java
+++
@@ -1,9 +0,0 @@
-import java.util.Date;
-import java.util.List;
-
-public abstract class Account {
-	private long ownerId; 
-	private Date startDate; 
-	private double balance;
-	private List<Operation> operations; 
-}
