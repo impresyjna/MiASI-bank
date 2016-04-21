e143ad9bf880d9017e1ce3bf2aa8ba4f6062cee1
wampir1408 <wampir1408@gmail.com>
2016-04-21 21:25:05 +0200

    Income operation tests

---
+++ src/test/java/operations/IncomeTest.java
@@ -0,0 +1,33 @@
+package operations;
+
+import accounts.Account;
+import junit.framework.TestCase;
+import org.junit.Before;
+import org.junit.Test;
+import util.User;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by impresyjna on 21.04.2016.
+ */
+public class IncomeTest extends TestCase {
+    private Account account;
+
+    @Before
+    public void setUp() throws Exception {
+        User user = new User("Zbigniew", "Testowy");
+        account = new Account(user, 1000, 0);
+    }
+
+    public void testAmountEqualsZero() throws Exception {
+        Income operation = new Income(account, 0);
+        assertFalse(operation.execute());
+    }
+
+    public void testCorrectIncomeOperation() throws Exception {
+        Income operation = new Income(account, 100);
+        assertTrue(operation.execute());
+        assertTrue(account.getBalance() == 1100);
+    }
+}
